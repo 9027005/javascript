1587f472418f11d2ff9dd162c0ebee34657aab54
9027005 <9027005@student.zadkine.nl>
2024-10-16 11:40:50 +0200

    opdracht 6 gemaakt

--- week3/opdracht6/script.js
+++ week3/opdracht6/script.js
@@ -1,5 +1,18 @@
-let txtContent = document.getElementById('txt')
-
-function calculate() {
-    
+function calculate(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
 }
+calculate(10,20)
+
+function multiplyByFive(num,num2) {
+    let multi = num * num2
+    console.log(multi)
+}
+multiplyByFive(5,12)
+
+function calcMinutes(num,num2) {
+    let mins = num / num2
+    console.log(mins)
+}
+
+calcMinutes(57134,60)
