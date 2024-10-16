e3c6f88860fe71786ced279283aae1af148ad557
9027005 <9027005@student.zadkine.nl>
2024-10-16 10:46:43 +0200

    te veel om over na te denken

---
+++ week2/app.js
@@ -0,0 +1,18 @@
+let heading = document.getElementById('txt');
+
+function changeTxt() {
+    heading.style.color = "green";
+    heading.innerText = "succes";
+}
+function txtBlue() {
+    heading.style.color = "blue"
+    heading.innerText = "Blauw"
+}
+function txtYellow() {
+    heading.style.color = "yellow"
+    heading.innerText = "Geel"
+}
+function txtRed() {
+    heading.style.color = "red"
+    heading.innerText = "Rood"
+}
