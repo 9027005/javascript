beca7b4187d709d8e87ea2b783188898e4201e6c
9027005 <9027005@student.zadkine.nl>
2024-10-23 11:01:51 +0200

    opdracht 8 gemaakt en temp bestand verwijderd

--- week4/opdracht8/app.js
+++ week4/opdracht8/app.js
@@ -1,11 +1,7 @@
+let product = document.getElementById("product")
 let budget = 100
-let prijs = 60
-let price = document.getElementById("price")
-let geld = document.getElementById("geld")
-let genoeg = document.getElementById("genoeg")
-
-price.innerText = prijs
-geld.innerText = budget
+let prijs = prompt()
+let genoeg = document.getElementById('genoeg')
 
 if(budget > prijs) {
     console.log("ik heb genoeg geld")
