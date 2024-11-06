7908a0c86b6baee0cfe345f0cafbdc9dedec086c
9027005 <9027005@student.zadkine.nl>
2024-11-06 11:12:37 +0100

    opdracht 11 gemaakt

--- week5/opdracht10/app.js
+++ week5/opdracht10/app.js
@@ -3,19 +3,35 @@ let num2 = document.getElementById("num2")
 let answ = document.getElementById("answ")
 
 function add() {
-    let product = Number(num1.value) + Number(num2.value)
-    answ.innerText = product
+    if (num1.value>0&&num2.value>0) {
+        let product = Number(num1.value) + Number(num2.value)
+        answ.innerText = product
+    } else {
+        answ.innerText = "getal is te laag"
+    }
 }
 function min() {
-    let product = num1.value - num2.value
-    answ.innerText = product
+    if (num1.value>0&&num2.value>0) {
+        let product = num1.value - num2.value
+        answ.innerText = product
+    } else {
+        answ.innerText = "getal is te laag"
+    }
 }
 function multi() {
-    let product = num1.value * num2.value
-    answ.innerText = product
+    if (num1.value>0&&num2.value>0) {
+        let product = num1.value * num2.value
+        answ.innerText = product
+    } else {
+        answ.innerText = "getal is te laag"
+    }
 }
 function devi() {
-    let product = num1.value / num2.value
-    answ.innerText = product
+    if (num1.value>0&&num2.value>0) {
+        let product = num1.value / num2.value
+        answ.innerText = product
+    } else {
+        answ.innerText = "getal is te laag"
+    }
 }
 
